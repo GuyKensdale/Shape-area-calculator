39d47ff1082061746e69716b9db9ce8d2b165c96
Guy <Guy@DESKTOP-9HFTHBM>
2024-07-19 10:25:41 +0100

    Add project files.

---
+++ Shape area calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_area_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+
+            Console.WriteLine("What shape would you like to find the area of? Please enter 'r' for a rectangle, 'T' for a right angled trinagle or any other key for a circle.");
+            
+            answer = Console.ReadLine();
+
+            if (answer == "r")
+            {
+                Console.WriteLine("Please entrer the height of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of the rectangle");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else if(answer == "t")
+            {
+                Console.WriteLine("Please entrer the height of the trinagle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of the triangle");
+                float width = float.Parse(Console.ReadLine());
+
+                result = (height + width) / 2;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the radius of the circle");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius *radius);
+            }
+
+            Console.WriteLine("The Area is " + result);
+            Console.ReadKey(); 
+        }
+    }
+}
